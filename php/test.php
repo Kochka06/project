2d564bc09dd94a5f4f55ba1da7b226a564b8fa98
Kochka06 <157406779+Kochka06@users.noreply.github.com>
2024-04-12 16:15:00 +0300

    Add files via upload

---
+++ php/test.php
@@ -0,0 +1,10 @@
+<?php // test1.php
+  $username = "Fred Smith";
+  echo $username;
+  echo "<br>";
+  $current_user = $username;
+  echo $current_user;
+  echo "<br>";
+  $hello = "Hello world!";
+  echo $hello;
+?>
